eb6a6eeca50cdf35e4fb5409b8e63699f5b23a5e
adamGT <adamgetachew55@gmail.com>
2026-04-09 22:22:46 +0300

    updated bot logic

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ require("dotenv").config();
 
 const TOK = process.env.TOKEN;
 const bot =  new Telegraf(TOK);
+const blockedUsers = new Set();
 
-bot.start((ctx) => ctx.reply('Welcome'))    
+bot.start((ctx) => ctx.reply('Welcome to our QR code generator bot. where you can send me any text and i will convert it to qr code. developed by madA'))    
 bot.use(async(ctx, next) => {
     getqrcode(ctx.update.message.text)
     setTimeout(function() {
@@ -23,6 +24,11 @@ bot.use(async(ctx, next) => {
     // await ctx.reply(JSON.stringify(getqrcode(ctx.update.message.text), null, 2));
     await ctx.replyWithPhoto({source: basesix})
 })
+
+bot.command('stop', async (ctx) => {
+  blockedUsers.add(ctx.from.id);
+  await ctx.reply("You have stopped the bot. Send /start to use it again.");
+});
 
 function getqrcode(text){
     QRCode.toDataURL(text, function (err, url) {
