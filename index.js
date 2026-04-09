1126b268d9f15a606ceec6fde96efe85088606ff
adamGT <adamgetachew55@gmail.com>
2026-04-09 22:37:17 +0300

    adding stop command trial one

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const bot =  new Telegraf(TOK);
 
 bot.start((ctx) => ctx.reply('Welcome to our QR code generator bot. where you can send me any text and i will convert it to qr code. developed by madA'))    
 
+bot.command('stop', async (ctx) => {  
+  await ctx.reply("You have stopped the bot. Send /start to use it again.");
+});
+
 bot.use(async(ctx, next) => {
     if (!ctx.update.message || !ctx.update.message.text) return;
     if (ctx.update.message.text.startsWith('/')) return;
@@ -23,14 +27,11 @@ bot.use(async(ctx, next) => {
 })
 
 bot.use(async(ctx, next) => {
-    // await ctx.reply(JSON.stringify(getqrcode(ctx.update.message.text), null, 2));
+    if (!ctx.update.message || !ctx.update.message.text) return;
+    if (ctx.update.message.text.startsWith('/')) return;
     await ctx.replyWithPhoto({source: basesix})
 })
 
-bot.command('stop', async (ctx) => {
-  
-  await ctx.reply("You have stopped the bot. Send /start to use it again.");
-});
 
 function getqrcode(text){
     QRCode.toDataURL(text, function (err, url) {
@@ -40,7 +41,6 @@ function getqrcode(text){
 }
 
 bot.launch()
-// module.exports = bot
 
 
 // Enable graceful stop
